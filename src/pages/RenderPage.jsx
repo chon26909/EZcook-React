2a70c2656061f8b5d6abe5805b4375e070b05353
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-17 22:13:26 +0700

    create render page

---
+++ src/pages/RenderPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RenderPage = () => {
+    return (
+        <div className='text-center'>
+            loading...
+        </div>
+    )
+}
+
+export default RenderPage
